efe7ceff264865ba141ac635c54c98286a32e222
n0ts <naoya.n@gmail.com>
2015-10-20 23:20:54 +0900

    Bump dnsmasq version to 2.75

--- spec/classes/dnsmasq_spec.rb
+++ spec/classes/dnsmasq_spec.rb
@@ -59,7 +59,7 @@ describe 'dnsmasq' do
     should contain_homebrew__formula('dnsmasq')
 
     should contain_package('boxen/brews/dnsmasq').with({
-      :ensure => '2.71-boxen1',
+      :ensure => '2.75-boxen1',
       :notify => "Service[#{servicename}]",
     })
 
